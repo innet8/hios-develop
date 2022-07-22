65fe112cfe1efca65f37344dd1b91535c6c5d254
aabb <aabb@sarzla.com>
2022-07-22 19:20:43 +0800

    no msg

--- run/constants.go
+++ run/constants.go
@@ -1,5 +1,12 @@
 package run
 
+import (
+	"bytes"
+	"github.com/innet8/hios/pkg/logger"
+	"strings"
+	"text/template"
+)
+
 const installBase = string(`#!/bin/bash
 #fonts color
 Green="\033[32m"
@@ -160,3 +167,29 @@ fi
 
 echo "success" > /tmp/.hicloud_installed
 `)
+
+func templateContent(templateContent string, envMap map[string]interface{}) string {
+	tmpl, err := template.New("text").Parse(templateContent)
+	defer func() {
+		if r := recover(); r != nil {
+			logger.Error("Template parse failed:", err)
+		}
+	}()
+	if err != nil {
+		panic(1)
+	}
+	var buffer bytes.Buffer
+	_ = tmpl.Execute(&buffer, envMap)
+	return string(buffer.Bytes())
+}
+
+func InstallBase(nodeName string) string {
+	var sb strings.Builder
+	sb.Write([]byte(installBase))
+	var envMap = make(map[string]interface{})
+	envMap["SERVER_URL"] = InConf.Server
+	envMap["NODE_NAME"] = nodeName
+	envMap["NODE_TOKEN"] = InConf.Token
+	envMap["SWAP_FILE"] = InConf.Swap
+	return templateContent(sb.String(), envMap)
+}
