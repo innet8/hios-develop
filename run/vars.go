d40a3ce79ab0ba448640c432760650c5a831f689
aabb <aabb@sarzla.com>
2022-07-24 15:05:52 +0800

    删除install，上传iso去除vy字符

--- run/vars.go
+++ run/vars.go
@@ -1,19 +1,9 @@
 package run
 
-type InJson struct {
-	Token  string
-	Mtu    string
-	Server string
-	Swap   string
-	Iver   string
-	Reset  bool
-}
-
 type WorkJson struct {
 	Server string
 }
 
 var (
-	InConf   InJson
 	WorkConf WorkJson
 )
