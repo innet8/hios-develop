5dbb2b41674b00b883b0f4925c660d9f327ea623
aabb <aabb@sarzla.com>
2022-09-01 11:46:04 +0800

    no message

--- data/constants.go
+++ data/constants.go
@@ -1,7 +1,11 @@
 package data
 
 const ExecContent = string(`#!/bin/bash
+echo "---------- cmd start ----------"
+
 {{.CMD}}
+
+echo "---------- cmd end ----------"
 
 if [ -n "{{.END_PATH}}" ]; then
 	echo "{{.END_TAG}}" > {{.END_PATH}}
