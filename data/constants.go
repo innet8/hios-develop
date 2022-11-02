70260104a6e257ce0ac11ab9d7256de24d412b45
aabb <aabb@sarzla.com>
2022-11-02 17:41:34 +0800

    no message

--- data/constants.go
+++ data/constants.go
@@ -5,7 +5,13 @@ echo "---------- cmd start ----------"
 
 {{.CMD}}
 
+CMD_RUN_RESULT=$?
+
 echo "---------- cmd end ----------"
+
+if [[ 0 -eq $CMD_RUN_RESULT ]]; then
+	exit 1
+fi
 
 if [ -n "{{.END_PATH}}" ]; then
 	echo "{{.END_TAG}}" > {{.END_PATH}}
